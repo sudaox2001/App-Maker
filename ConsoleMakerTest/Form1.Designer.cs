1ba53f66f8aba8ac8a0534accadc2640ada36d47
sudaox2001 <107582771+sudaox2001@users.noreply.github.com>
2022-07-24 19:15:28 +0100

    Add files via upload

---
+++ ConsoleMakerTest/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ConsoleMakerTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.conW = new System.Windows.Forms.RadioButton();
+            this.diaW = new System.Windows.Forms.RadioButton();
+            this.ee1 = new System.Windows.Forms.Timer(this.components);
+            this.ee2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 60);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(260, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(102, 86);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Compile";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // conW
+            // 
+            this.conW.AutoSize = true;
+            this.conW.Location = new System.Drawing.Point(12, 13);
+            this.conW.Name = "conW";
+            this.conW.Size = new System.Drawing.Size(105, 17);
+            this.conW.TabIndex = 2;
+            this.conW.TabStop = true;
+            this.conW.Text = "Console Window";
+            this.conW.UseVisualStyleBackColor = true;
+            // 
+            // diaW
+            // 
+            this.diaW.AutoSize = true;
+            this.diaW.Location = new System.Drawing.Point(12, 37);
+            this.diaW.Name = "diaW";
+            this.diaW.Size = new System.Drawing.Size(55, 17);
+            this.diaW.TabIndex = 3;
+            this.diaW.TabStop = true;
+            this.diaW.Text = "Dialog";
+            this.diaW.UseVisualStyleBackColor = true;
+            // 
+            // ee1
+            // 
+            this.ee1.Enabled = true;
+            this.ee1.Interval = 30000;
+            this.ee1.Tick += new System.EventHandler(this.ee1_Tick);
+            // 
+            // ee2
+            // 
+            this.ee2.Interval = 1000;
+            this.ee2.Tick += new System.EventHandler(this.ee2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 112);
+            this.Controls.Add(this.diaW);
+            this.Controls.Add(this.conW);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton conW;
+        private System.Windows.Forms.RadioButton diaW;
+        private System.Windows.Forms.Timer ee1;
+        private System.Windows.Forms.Timer ee2;
+    }
+}
+
---
+++ ConsoleMakerTest/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace ConsoleMakerTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string cscExec = System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory() + "csc.exe";
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //if(!File.Exists("cons.cs"))
+            //{
+            //    MessageBox.Show("Template file does not exist. Compiling has been disabled");
+            //    button1.Enabled = false;
+            //    button1.Text = "Compile Disabled";
+            //}
+            //MessageBox.Show(netfwDir);
+            //MessageBox.Show(cscExec);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string eConsole = File.ReadAllText("cons.cs");
+            if (conW.Checked)
+            {
+                if (File.Exists("ConsoleApp.exe"))
+                {
+                    File.Delete("ConsoleApp.exe");
+                }
+                string eConsole = Properties.Resources.cons;
+                string nConsole = eConsole.Replace("______replmw", textBox1.Text);
+                nConsole = nConsole.Replace(@"\""", "");
+                File.WriteAllText("ConsoleApp.cs", nConsole);
+                File.WriteAllText("AssemblyInfo.cs", Properties.Resources.AssemblyInfo);
+                //Process proc = Process.Start(cscExec, "ConsoleApp.cs AssemblyInfo.cs");
+                Process proc = new Process();
+                proc.StartInfo.FileName = cscExec;
+                proc.StartInfo.Arguments = "ConsoleApp.cs AssemblyInfo.cs";
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.Start();
+                proc.WaitForExit();
+                File.Delete("ConsoleApp.cs");
+                File.Delete("AssemblyInfo.cs");
+                if (File.Exists("ConsoleApp.exe"))
+                {
+                    Process proc2 = Process.Start("ConsoleApp.exe");
+                    proc2.WaitForExit();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to compile");
+                }
+            }
+            if (diaW.Checked)
+            {
+                if (File.Exists("DialogApp.exe"))
+                {
+                    File.Delete("DialogApp.exe");
+                }
+                string eConsole = Properties.Resources.diag;
+                string nConsole = eConsole.Replace("______replmw", textBox1.Text);
+                File.WriteAllText("DialogApp.cs", nConsole);
+                File.WriteAllText("AssemblyInfo.cs", Properties.Resources.AssemblyInfo);
+                Process proc = new Process();
+                proc.StartInfo.FileName = cscExec;
+                proc.StartInfo.Arguments = "DialogApp.cs AssemblyInfo.cs";
+                proc.StartInfo.UseShellExecute = false;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.Start();
+                proc.WaitForExit();
+                File.Delete("DialogApp.cs");
+                File.Delete("AssemblyInfo.cs");
+                if (File.Exists("DialogApp.exe"))
+                {
+                    Process proc2 = Process.Start("DialogApp.exe");
+                    proc2.WaitForExit();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to compile");
+                }
+            }
+        }
+
+        private void ee1_Tick(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+            ee2.Enabled = true;
+        }
+
+        private void ee2_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int sW = Screen.PrimaryScreen.WorkingArea.Width;
+            int sH = Screen.PrimaryScreen.WorkingArea.Height;
+            int nWT = rnd.Next(0, sW);
+            int nHT = rnd.Next(0, sH);
+            this.Location = new Point(sW, sH);
+        }
+    }
+}
